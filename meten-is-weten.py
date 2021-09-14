d0d0e18983da3cff55e357effeedf89ce7005acd
Thomas Koman <99066789@mydavinci.nl>
2021-09-14 10:21:34 +0200

    elif-statement

---
+++ meten-is-weten.py
@@ -0,0 +1,9 @@
+a = int(input("Geef me een heel getal: "))
+b = int(input("Geef me nog een heel getal: "))
+
+if a > b:
+    max = a
+    print(f"a is het grootste getal: {max}")
+elif a < b:
+    min = a
+    print(f"a is het kleinste getal: {min}")
